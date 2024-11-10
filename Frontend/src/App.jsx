09e14ae35e42a19b3e40349306f8101ed2bf6724
amaan92001 <113185353+amaan92001@users.noreply.github.com>
2024-11-10 23:24:34 +0530

    uploaded frontend backend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from "./componets/Sidebar.jsx"
+import MusicPlay from "./componets/MusicPlay.jsx"
+
+function App() {
+  return (
+    <>
+
+  <Sidebar/>
+      <MusicPlay/>
+    </>
+  );
+}
+
+export default App;
